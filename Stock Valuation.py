483acba07a340441a643d2babab61fa8fb148265
Tomas Miele <tomasrm@al.insper.edu.br>
2025-07-18 19:25:21 -0300

    feat: calculate the growth

--- Stock Valuation.py
+++ Stock Valuation.py
@@ -5,6 +5,7 @@ import ast
 import yfinance as yf
 import statistics
 import pandas as pd
+import re
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
@@ -14,7 +15,7 @@ client = OpenAI(api_key=api_key)
 tickers_info = {}
 
 # Get the stock to be analysed
-ticker = input("Type the stock ticker you want to evaluate: ").upper()
+ticker = input("Type the stock you want to evaluate (e.g. 'KO'): ").upper()
 
 stock = yf.Ticker(ticker)
 
@@ -29,7 +30,7 @@ prompt = f"""prompt = f'List 4 US-listed companies in the "{industry}" industry,
 with valuation multiples available on yfinance. Output only a Python list of tickers."""
 
 # response = client.chat.completions.create(
-#     model="gpt-4",
+#     model="gpt-4o",
 #     messages=[
 #         {"role": "user", "content": prompt}
 #     ]
@@ -43,12 +44,16 @@ tickers_list = ["PEP", "KDP", "MNST", "CCEP"]
 for t in tickers_list:
     tickers_info[t] = {}
 
+# 1st Method - PE Multiples
+
 # Calculate the trailing and forward PE of the stock
 trailing_PE = round(ticker_info.get("trailingPE"), 2)
 forward_PE = round(ticker_info.get("forwardPE"), 2)
 
-tickers_info[ticker]["Trailing PE"] = trailing_PE
-tickers_info[ticker]["Forward PE"] = forward_PE
+tickers_info[ticker]["Multiple"] = {}
+
+tickers_info[ticker]["Multiple"]["Trailing PE"] = trailing_PE
+tickers_info[ticker]["Multiple"]["Forward PE"] = forward_PE
 
 # Calculate the average trailing PE of the last 5y
 ticker_price = stock.history(period="5y")["Close"].resample("YE").last()
@@ -72,7 +77,7 @@ combined = combined.dropna()
 
 ticker_five_y_avg_trailing_pe = float(round(combined["Trailing PE"].mean(), 2))
 
-tickers_info[ticker]["5y Avg Trailing PE"] = ticker_five_y_avg_trailing_pe
+tickers_info[ticker]["Multiple"]["5y Avg Trailing PE"] = ticker_five_y_avg_trailing_pe
 
 # Repeat the process to the others companies
 for t in tickers_list:
@@ -82,8 +87,10 @@ for t in tickers_list:
     peers_trailing_PE = round(peers_info.get("trailingPE"), 2)
     peers_forward_PE = round(peers_info.get("forwardPE"), 2)
 
-    tickers_info[t]["Trailing PE"] = peers_trailing_PE
-    tickers_info[t]["Forward PE"] = peers_forward_PE
+    tickers_info[t]["Multiple"] = {}
+
+    tickers_info[t]["Multiple"]["Trailing PE"] = peers_trailing_PE
+    tickers_info[t]["Multiple"]["Forward PE"] = peers_forward_PE
 
     peers_price = peers.history(period="5y")["Close"].resample("YE").last()
     peers_price.index = peers_price.index.tz_localize(None)
@@ -106,11 +113,11 @@ for t in tickers_list:
 
     peers_five_y_avg_trailing_pe = float(round(peers_combined["Trailing PE"].mean(), 2))
 
-    tickers_info[t]["5y Avg Trailing PE"] = peers_five_y_avg_trailing_pe
+    tickers_info[t]["Multiple"]["5y Avg Trailing PE"] = peers_five_y_avg_trailing_pe
 
 # Calculate the average and mean PE's from the 5 companies combined
-trailing_pes = [v['Trailing PE'] for v in tickers_info.values()]
-forward_pes = [v['Forward PE'] for v in tickers_info.values()]
+trailing_pes = [v["Multiple"]['Trailing PE'] for v in tickers_info.values()]
+forward_pes = [v["Multiple"]['Forward PE'] for v in tickers_info.values()]
 
 avg_trailing_PE = round(statistics.mean(trailing_pes), 2)
 median_trailing_PE = round(statistics.median(trailing_pes), 2)
@@ -118,17 +125,105 @@ avg_forward_PE = round(statistics.mean(forward_pes), 2)
 median_forward_PE = round(statistics.median(forward_pes), 2)
 
 # Calculate the difference between the average and median for each company
-tickers_info[ticker]["PE Diff Indust Avg"] = round(avg_trailing_PE / tickers_info[ticker]["Trailing PE"] * 100 - 100, 2)
-tickers_info[ticker]["PE Diff Indust Median"] = round(median_trailing_PE / tickers_info[ticker]["Trailing PE"] * 100 - 100, 2)
-tickers_info[ticker]["Forward PE Diff Indust Avg"] = round(avg_forward_PE / tickers_info[ticker]["Forward PE"] * 100 - 100, 2)
-tickers_info[ticker]["Forward PE Diff Indust Median"] = round(median_forward_PE / tickers_info[ticker]["Forward PE"] * 100 - 100, 2)
-tickers_info[ticker]["5y PE Diff"] = round(tickers_info[ticker]["5y Avg Trailing PE"] / tickers_info[ticker]["Trailing PE"] * 100 - 100, 2)
+tickers_info[ticker]["Multiple"]["PE Diff Indust Avg"] = round(avg_trailing_PE / tickers_info[ticker]["Multiple"]["Trailing PE"] * 100 - 100, 2)
+tickers_info[ticker]["Multiple"]["PE Diff Indust Median"] = round(median_trailing_PE / tickers_info[ticker]["Multiple"]["Trailing PE"] * 100 - 100, 2)
+tickers_info[ticker]["Multiple"]["Forward PE Diff Indust Avg"] = round(avg_forward_PE / tickers_info[ticker]["Multiple"]["Forward PE"] * 100 - 100, 2)
+tickers_info[ticker]["Multiple"]["Forward PE Diff Indust Median"] = round(median_forward_PE / tickers_info[ticker]["Multiple"]["Forward PE"] * 100 - 100, 2)
+tickers_info[ticker]["Multiple"]["5y PE Diff"] = round(tickers_info[ticker]["Multiple"]["5y Avg Trailing PE"] / tickers_info[ticker]["Multiple"]["Trailing PE"] * 100 - 100, 2)
 
 for t in tickers_list:
-    tickers_info[t]["PE Diff Indust Avg"] = round(avg_trailing_PE / tickers_info[t]["Trailing PE"] * 100 - 100, 2)
-    tickers_info[t]["PE Diff Indust Median"] = round(median_trailing_PE / tickers_info[t]["Trailing PE"] * 100 - 100, 2)
-    tickers_info[t]["Forward PE Diff Indust Avg"] = round(avg_forward_PE / tickers_info[t]["Forward PE"] * 100 - 100, 2)
-    tickers_info[t]["Forward PE Diff Indust Median"] = round(median_forward_PE / tickers_info[t]["Forward PE"] * 100 - 100, 2)
-    tickers_info[t]["5y PE Diff"] = round(tickers_info[t]["5y Avg Trailing PE"] / tickers_info[t]["Trailing PE"] * 100 - 100, 2)
+    tickers_info[t]["Multiple"]["PE Diff Indust Avg"] = round(avg_trailing_PE / tickers_info[t]["Multiple"]["Trailing PE"] * 100 - 100, 2)
+    tickers_info[t]["Multiple"]["PE Diff Indust Median"] = round(median_trailing_PE / tickers_info[t]["Multiple"]["Trailing PE"] * 100 - 100, 2)
+    tickers_info[t]["Multiple"]["Forward PE Diff Indust Avg"] = round(avg_forward_PE / tickers_info[t]["Multiple"]["Forward PE"] * 100 - 100, 2)
+    tickers_info[t]["Multiple"]["Forward PE Diff Indust Median"] = round(median_forward_PE / tickers_info[t]["Multiple"]["Forward PE"] * 100 - 100, 2)
+    tickers_info[t]["Multiple"]["5y PE Diff"] = round(tickers_info[t]["Multiple"]["5y Avg Trailing PE"] / tickers_info[t]["Multiple"]["Trailing PE"] * 100 - 100, 2)
 
-print(tickers_info)
+# 2nd Method - DCF
+
+# Calculate the cashflow
+tickers_info[ticker]["DCF"] = {}
+
+cashflow = stock.cashflow
+
+try:
+    last_4_years = cashflow.columns[:4]
+except Exception as e:
+    print(f"[{ticker}] Erro ao acessar colunas do cashflow: {e}")
+
+if "Free Cash Flow" in cashflow.index:
+    fcf = cashflow.loc["Free Cash Flow", last_4_years]
+else:
+    try:
+        ocf = cashflow.loc["Operating Cash Flow", last_4_years]
+        capex = cashflow.loc["Capital Expenditures", last_4_years]
+        fcf = ocf - capex
+    except KeyError as e:
+        print(f"[{ticker}] Dados insuficientes para calcular FCF: {e}")
+
+fcf = fcf.sort_index()
+
+tickers_info[ticker]["DCF"]["FCF"] = fcf
+
+y0 = fcf.iloc[-3]
+y1 = fcf.iloc[-2]
+y2 = fcf.iloc[-1]
+
+drop_ratio = (y1 - y2) / y1
+
+if drop_ratio > 0.3:
+    prev_growth = y1 / y0
+    adjusted_y2 = y1 * prev_growth
+    fcf.iloc[-1] = adjusted_y2
+
+tickers_info[ticker]["DCF"]["Ajusted FCF"] = fcf
+
+# Calculate the growth
+fcf_growth = round(fcf.sort_index().pct_change().dropna() * 100, 2)
+historical_growth = fcf_growth.mean()
+
+historical_growth = 3.50
+
+cagr_prompt = f"""Search for the forecasted CAGR of the "{industry}" industry **specifically** 
+from either Grand View Research (GVR) or Statista. If available, return **only** the CAGR number. 
+Do not include any units, percent signs, or explanation."""
+
+# cagr_response = client.chat.completions.create(
+#     model="gpt-4o",
+#     messages=[
+#         {"role": "user", "content": cagr_prompt}
+#     ]
+# )
+
+# cagr = cagr_response.choices[0].message.content.strip()
+
+# match = re.search(r"[\d.]+", cagr)
+# if match:
+#     cagr = float(match.group())
+
+cagr = 6.40
+
+estimate_growth = 0.8 * historical_growth + 0.2 * cagr
+
+tickers_info[ticker]["DCF"]["Historical Growth"] = historical_growth
+tickers_info[ticker]["DCF"]["CAGR"] = cagr
+tickers_info[ticker]["DCF"]["Estimate Growth"] = estimate_growth
+
+# Forecast
+projected_fcf = []
+last_fcf = fcf.iloc[-1]
+
+for i in range(1, 11):
+    projected_value = last_fcf * ((1 + estimate_growth / 100) ** i)
+    projected_fcf.append(float(round(projected_value, 2)))
+
+print(projected_fcf)
+
+p_fcf = ticker_info["marketCap"] / fcf
+
+print(p_fcf.median())
+
+tickers_info[ticker]["DCF"]["Projected FCF"] = projected_fcf
+
+# Repeat the process to the others companies
+
+#print(tickers_info)
